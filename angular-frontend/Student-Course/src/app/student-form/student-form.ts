7e66234c7227877141988f4556f2ca3596f14fa3
NL.A <james342506@gmail.com>
2025-08-29 00:27:01 +0630

    alert box

--- angular-frontend/Student-Course/src/app/student-form/student-form.ts
+++ angular-frontend/Student-Course/src/app/student-form/student-form.ts
@@ -1,56 +1,54 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 import { StudentService } from '../service/student-service';
 import { Router, RouterModule } from '@angular/router';
 import { Student } from '../model/Student';
 import { FormsModule, NgForm } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-student-form',
   standalone: true,
-  imports: [RouterModule,FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './student-form.html',
-  styleUrls: ['./student-form.css'] 
+  styleUrls: ['./student-form.css'],
 })
 export class StudentForm {
-
-  students:Student = {
+  students: Student = {
     name: '',
     email: '',
     age: 0,
     phone: '',
     address: '',
     gender: '',
-    status: ''
-  }
-  successMessage:string=''
+    status: '',
+  };
+  successMessage: string = '';
 
-  constructor(private studentService:StudentService,private router:Router){}
-
-  onSubmit(studentForm:NgForm) {
-
-
-   const newStudent: Student = { ...this.students };
+  constructor(private studentService: StudentService, private router: Router,private cdRef: ChangeDetectorRef) {}
+onSubmit(studentForm: NgForm) {
+  if (studentForm.valid) {
+    const newStudent: Student = { ...this.students };
 
     this.studentService.addStudent(newStudent).subscribe({
       next: student => {
         console.log('Student added successfully:', student);
-        this.successMessage="Student added successfully"
-        this.router.navigateByUrl('/student-list')
+        this.successMessage = "Student added successfully";
+
+        // 👇 Force Angular to detect changes
+        this.cdRef.detectChanges();
+
+        // Delay navigation to show alert
+        setTimeout(() => {
+          this.router.navigateByUrl('/student-list');
+        }, 2000);
       },
       error: err => {
         console.error('Error adding student:', err);
-      },
-      complete: () => {
-        console.log('Student added successfully');
       }
-      
-  });
+    });
   }
+}
 
-  
-    
-  
 
 }
